eb65424121388fa662bb5429efcd0198fb2046b5
alsidneio <alsidneio@gmail.com>
2018-11-23 11:01:04 -0800

    pushing to save data

--- SPA/index.js
+++ SPA/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const events = require('./public/scripts/Events')
 
 //public folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+
 const port= 4000;
+
 
 app.listen(port, ()=> console.log(`Web Server started on port: ${port}`));
