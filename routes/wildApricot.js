c26bec06d97d5deff9507d48ee02c74ab49bc719
abell <abell@meteorcomm.com>
2020-05-17 16:14:52 -0700

    saving spot to do some work for an upcoming meeting

--- routes/wildApricot.js
+++ routes/wildApricot.js
@@ -1,5 +1,8 @@
 const axios = require('axios').default;
 const qs = require('qs');
+const dotenv = require('dotenv').config();
+
+const apiBaseUrl = 'https://api.wildapricot.org/v2/';
 
 axios({
     method: 'post',
@@ -8,21 +11,53 @@ axios({
    
     headers: {
         'Content-type': 'application/x-www-form-urlencoded',
-        'Authorization': "Basic MzAwNU1lZ2Fab3JkOmc0c29icmc0NHFiNTNsbmMzd2tnbTNvdGltOWNidQ==",
+        'Authorization': `Basic ${process.env.WA_AUTH_CODE}`,
         'Accept':'application/json',
-        'Host': 'oauth.wildapricot.org'
+        
     },
     data:qs.stringify({
         grant_type:'password',
-        username:'alsidneio@gmail.com',
-        password:'Mookie23!',
+        username:process.env.WA_USER,
+        password:process.env.WA_PASS,
         scope:'auto',        
     })
 }).then(response => { 
     console.log('Success');
-	console.log(response.data);
+    console.log(response.data);
+    listEvents(response.data.access_token)
 })
 .catch(error => {
     console.log('Error');
     console.log(error.response.data);
 });
+
+function listEvents(authToken) {
+    axios({
+        method:'get',
+        url:`${apiBaseUrl}/accounts/${process.env.WA_ACCT_ID}/events`,
+    }, {
+        headers: {
+          'Accept': 'application/json',
+          "Authorization": `Bearer ${authToken}`
+        },
+        qs: {
+          $async: false,
+          $filter: `'First name' eq '${searchString}'`,
+          $select: "'First name'"
+        },
+        json: true
+      },
+      function( err, res, body) {
+        if( err) {
+          console.error(err);
+          throw err;
+        }
+        else {
+          for (var i = 0; i < body.Contacts.length; i++) {
+            let contact = body.Contacts[i];
+            console.log(`${contact.DisplayName}: ${contact.Email}`);
+          }
+        }
+      }
+    );
+}
